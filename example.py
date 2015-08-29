33341326ec0d9ddfd283d9ce01dfb666dca33bcb
Rob Berry <rob@luvhate.us>
2015-08-29 16:10:42 +0100

    Determine changes to be made to given tile
    Given a set of directions and start point, we create a list of tuples
    representing the changes to be made to a tile's position e.g. one move south
    == (0, -1)

--- example.py
+++ example.py
@@ -1,5 +1,10 @@
+# coding=utf-8
+
 import re
+import sys
+import operator
 import functools
+from pprint import pprint
 
 
 class Position(object):
@@ -10,22 +15,38 @@ class Position(object):
     def __repr__(self):
         return u'{}x{}'.format(self.x, self.y)
 
+    @property
+    def as_tuple(self):
+        return (self.x, self.y)
+
+    def __eq__(self, other):
+        return self.as_tuple == other.as_tuple
+
 
 class Empty(object):
     def __init__(self, position):
         self.position = position
 
     def __repr__(self):
-        return u'<{} {}>'.format('Empty', self.position)
+        return u'___'
 
 
 class Ship(object):
     def __init__(self, position, direction=u'N'):
         self.position = position
         self.direction = direction
+        self.sunk = False
 
     def __repr__(self):
-        return u'<{} {}{}>'.format('Ship', self.position, self.direction)
+        delim = u'\{}/' if self.sunk else u'_{}_'
+        return delim.format(self.direction)
+
+    @property
+    def position_tuple(self):
+        return self.position.as_tuple
+
+    def sink(self):
+        self.sunk = True
 
 
 def make_tile(kind, position, **extra):
@@ -41,16 +62,24 @@ def make_ship(s):
     return make_tile('ship', Position(*args[:2]), direction=args[2])
 
 
+def shoot_at_ship(ship, shot_position):
+    if ship.position == shot_position:
+        ship.sink()
+
+
 def make_grid(x, y):
-    return [[make_tile('empty', Position(x1, y1)) for x1 in range(x)] for y1 in range(y)]
+    return [[make_tile('empty', Position(x1, y1)) for x1 in range(x)] for y1 in range(y)][::-1]
 
 
 def position_tile(grid, tile, position):
     # ensure that the x and y coords don't exceed the grid dimensions
+    grid = grid[::-1]
     y = max(min(len(grid) - 1, position.y), 0)
     x = max(min(len(grid[y]) - 1, position.x), 0)
     grid[y][x] = tile
-    return grid
+    tile.position.x = x
+    tile.position.y = y
+    return grid[::-1]
 
 
 ws_regex = re.compile(r'\s{1,}')
@@ -83,4 +112,60 @@ def get_ship_action(s):
         return {}
     target, movements = m.groups()
     pos = Position(*map(int, target.split(',')))
-    return {'position': pos, 'action': 'shot' if movements is None else 'movement'}
+    return {'position': pos,
+            'action': 'shot' if movements is None else 'movement',
+            'movements': movements or ''}
+
+
+def rotate(direction, rotate):
+    directions = {'N': ['W', 'E'],
+                  'W': ['S', 'N'],
+                  'S': ['E', 'W'],
+                  'E': ['N', 'S']}
+    index = 0 if rotate == 'L' else 1
+    return directions[direction][index]
+
+
+def get_coords_from_movements(movements, direction='N'):
+    changes = []
+    for c in movements:
+        if c in ['L', 'R']:
+            direction = rotate(direction, c)
+            continue
+        if direction == 'N':
+            change = (0, 1)
+        elif direction == 'S':
+            change = (0, -1)
+        elif direction == 'W':
+            change = (-1, 0)
+        else:
+            change = (1, 0)
+        changes.append(change)
+    return changes
+
+
+def change_position(position, change):
+    x, y = position.x, position.y
+    return Position(*[operator.add(*pair) for pair in zip((x, y), change)])
+
+
+def add_tuples(a, b):
+    return tuple([operator.add(*pair) for pair in zip(a, b)])
+
+
+def get_tile_from_grid(grid, target):
+    return grid[::-1][target.y][target.x]
+
+
+def final_standings(grid):
+    retval = []
+    for yindex, y in enumerate(grid[::-1]):
+        for xindex, tile in enumerate(y):
+            if not isinstance(tile, Ship):
+                continue
+            args = [xindex,
+                    yindex,
+                    tile.direction,
+                    ' SUNK' if tile.sunk else '']
+            retval.append('({}, {}, {}){}'.format(*args))
+    return '\n'.join(retval[::-1])
--- tests/test_grid.py
+++ tests/test_grid.py
@@ -23,12 +23,12 @@ def test_position_tile(five_grid):
     position = Position(2, 3)
     tile = make_tile('ship', position)
     grid = position_tile(five_grid, tile, position)
-    assert grid[3][2] == tile
+    assert grid[1][2] == tile
 
 
 @pytest.mark.parametrize(("position", "x", "y"), [
-    ((6, 10), 4, 4),
-    ((-6, -10), 0, 0),
+    ((6, 10), 4, 0),
+    ((-6, -10), 0, 4),
 ])
 def test_cannot_position_tile_beyond_grid(position, x, y):
     from example import position_tile, Position, make_tile, make_grid
@@ -59,3 +59,47 @@ def test_ship_action(action_string, action_kind):
     from example import get_ship_action
     result = get_ship_action(action_string)
     assert action_kind == result['action']
+
+
+@pytest.mark.parametrize(("movement", "expected"), [
+    (u'MRMLMM', [(0, 1), (1, 0), (0, 1), (0, 1)]),
+])
+def test_convert_movement_to_coords(movement, expected):
+    from example import get_coords_from_movements
+    assert get_coords_from_movements(movement) == expected
+
+
+@pytest.mark.parametrize(("current", "angle", "new"), [
+    (u'N', u'R', u'E'),
+    (u'N', u'L', u'W'),
+    (u'S', u'L', u'E'),
+    (u'S', u'R', u'W'),
+    (u'E', u'L', u'N'),
+    (u'E', u'R', u'S'),
+    (u'W', u'L', u'S'),
+    (u'W', u'R', u'N'),
+])
+def test_rotate(current, angle, new):
+    from example import rotate
+    assert rotate(current, angle) == new
+
+
+@pytest.mark.parametrize(("old", "new", "expected"), [
+    ((10, 12), (7, 4), (17, 16)),
+    ((10, 8), (-7, 0), (3, 8)),
+])
+def test_change_position(old, new, expected):
+    from example import Position, change_position
+    pos = Position(*old)
+    new_pos = change_position(pos, new)
+    assert (new_pos.x, new_pos.y) == expected
+
+
+def test_final_standings(five_grid):
+    from example import Position, make_tile, position_tile, final_standings
+    position = Position(2, 3)
+    tile = make_tile('ship', position, direction=u'E')
+    grid = position_tile(five_grid, tile, position)
+
+    result = final_standings(grid)
+    assert result == u'(2, 3, E)'
